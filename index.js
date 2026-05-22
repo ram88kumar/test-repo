8fe7847e9ec99acbc93e1f4366295d95d74fc26d
Ram88Kumar <shreeram940@gmail.com>
2026-05-22 14:38:45 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route for GET requests to the root URL
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Ram. Date: ' + new Date() + '</h1');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
